c828f43045e26965e2c5ce0b256a8dd2307514ac
Alejandro Serrano <92839762+SerranoH93@users.noreply.github.com>
2024-05-24 19:02:05 -0600

    Se termina formulario

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,67 @@
 "use client"
 
 import { useForm } from "react-hook-form"; 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { userSchema, mappedPlans } from "@/validations/userSchema";
 
+type Inputs = {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;  
+  weight: string;
+  plan: string;
+  dateOfBirth: string;
+}
 
 function Home() {
-  const {register, handleSubmit} = useForm();
+  const {register, handleSubmit, watch, formState: {errors}} = useForm<Inputs>({
+    resolver: zodResolver(userSchema)
+  });
+
+  const plansOptions = Object.entries(mappedPlans).map(([key, value]) => {
+    return <option value={key} key={key}>{value}</option>
+  })
+
+  console.log(errors)
 
   return (
     <div>
       <form onSubmit={handleSubmit(data => {console.log(data)})}>
         <label htmlFor="name">Name</label>
         <input type="text" id="name" {...register("name")}/>
+        {errors.name?.message && <p>{errors.name?.message}</p>}
 
         <label htmlFor="email">Email</label>
         <input type="email" id="email" {...register("email")}/>
+        {errors.email?.message && <p>{errors.email?.message}</p>}
 
         <label htmlFor="password">Password</label>
         <input type="password" id="password" {...register("password")}/>
+        {errors.password?.message && <p>{errors.password?.message}</p>}
 
         <label htmlFor="confirmPassword">Confirm Password</label>
         <input type="password" id="confirmPassword" {...register("confirmPassword")}/>
+        {errors.confirmPassword?.message && <p>{errors.confirmPassword?.message}</p>}
 
         <label htmlFor="dateOfBirth">Date of Birth</label>
         <input type="date" id="dateOfBirth" {...register("dateOfBirth")}/>
 
         <label htmlFor="weight">Weight</label>
-        <input type="text" id="weight" {...register("weigth")}/>
+        <input type="text" id="weight" {...register("weight")}/>
+        {errors.weight?.message && <p>{errors.weight?.message}</p>}
 
         <label htmlFor="plan">Plan</label>
         <select id="plan" {...register("plan")}>
-          <option value="free">Free</option>
-          <option value="basic">Basic</option>
-          <option value="medium">Medium</option>
-          <option value="premium">Premium</option>
+          {plansOptions}
         </select>
+        {errors.plan?.message && <p>{errors.plan?.message}</p>}
 
         <button type="submit">Submit</button>
       </form>
+      <div>
+        {JSON.stringify(watch(), null, 2)}
+      </div>
     </div>
   )
 }
--- src/validations/userSchema.ts
+++ src/validations/userSchema.ts
@@ -1,12 +1,37 @@
 import {z} from "zod";
 
-const userSchema = z.object({
+const plans = ['free', 'basic', 'medium', 'premium'] as const;
+
+export type Plans = (typeof plans)[number];
+
+export const mappedPlans: {[key in Plans]: string} = {
+    basic: "Basic",
+    free: "Free",
+    medium: "Medium",
+    premium: "Premium"
+}
+
+export const userSchema = z.object({
     name: z.string().min(3, {
         message: 'Name must be at least 3 characters long'
     }).max(200, {
-        message: 'Name must be less that 200 characters long'
+        message: 'Name must be max 200 characters long'
     }),
     email: z.string().email({
         message: 'Please enter a valid email'
+    }),
+    password: z.string().min(8, {
+        message: 'Password must be at least 3 characters long'
+    }).max(50, {
+        message: 'Password must be max 50 characters long'}),
+    confirmPassword: z.string().min(8, {
+        message: 'Password must be at least 3 characters long'
+    }).max(50, {
+        message: 'Password must be max 50 characters long'}),
+    weight: z.string().refine((weight) => !isNaN(parseFloat(weight)), {
+        message: 'Weight must be a number'
+    }),
+    plan: z.enum(plans, {
+        errorMap: () => ({message: 'Please select a plan'})
     })
 })
